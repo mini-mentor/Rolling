679bbbbc2c1fe391b2a63c97442ca88d3dace215
haeunNoh06 <okhagrace06@naver.com>
2023-06-08 21:42:26 +0900

    git init

---
+++ src/main/java/me/rolling/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,8 @@
+package me.rolling.springbootdeveloper.repository;
+
+import me.rolling.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+
+}
