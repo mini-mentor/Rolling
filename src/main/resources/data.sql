a17799d28df99332efb5a52276948fbe3ede6437
haeunNoh06 <okhagrace06@naver.com>
2023-11-09 22:44:46 +0900

    :bug: [refactor] mysql의 값이 저장, 조회가 되지 않음

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO rollingpaper (maker, receiver, title) VALUES ('김길동', '1-1반', '우리의 졸업을 축하합니다.')
-INSERT INTO paper (content, writer, title) VALUES ('졸업 축하해 수고많았어', '김길동', '헤이헤이')
+INSERT INTO paper (content, writer, title) VALUES ('content1', 'writer1', 'title1')
